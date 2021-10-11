2e659363f112c23738c8661e6714cd2dffc43f8c
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2021-10-11 21:44:46 +0530

    Binary Tree Right View solution correction

--- 14. Tree/BinaryTreeRightSideView.cpp
+++ 14. Tree/BinaryTreeRightSideView.cpp
@@ -1,48 +1,143 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int>v;
-    if(!root)
-    return v;
-    queue<TreeNode*>q;
-    bool isFirst=true;
-    q.push(root);
-    q.push(NULL);
-    while(!q.empty())
-    {
-        TreeNode* temp=q.front();
-        q.pop();
-        if(!temp)
-        {
-           if(q.size()==0)
-           break;
-           q.push(NULL);
-           isFirst=true;
-        }
-        else
-        {
-           if(isFirst)
-           {
-               v.push_back(temp->val);
-               isFirst=false;
-           }
-        if(temp->right)
-        q.push(temp->right);
-        if(temp->left)
-        q.push(temp->left);
-        }
-    }
-    return v;
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
 };
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+class Solution
+{
+    public:
+  
+    vector<int> rightView(Node *root)
+    {
+      
+       vector<int>v;
+       if(root==NULL){
+           return v;
+       }
+       queue<Node*>q;
+       q.push(root);
+      
+      while(!q.empty()){
+          int n=q.size();
+          for(int i=0; i<n; i++){
+              Node* curr=q.front();
+              q.pop();
+              if(i==n-1){
+                  v.push_back(curr->data);
+              }
+              if(curr->left!=NULL){
+                  q.push(curr->left);
+              }
+              if(curr->right!=NULL){
+                  q.push(curr->right);
+              }
+          }
+      }
+    return v;
+    }
+       
+};
+
+
+
+
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+ 
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+
+    Node* root = newNode(stoi(ip[0]));
+
+    
+    queue<Node*> queue;
+    queue.push(root);
+
+
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+     
+        Node* currNode = queue.front();
+        queue.pop();
+
+       
+        string currVal = ip[i];
+
+   
+        if(currVal != "N") {
+
+          
+            currNode->left = newNode(stoi(currVal));
+
+         
+            queue.push(currNode->left);
+        }
+
+      
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        
+        if(currVal != "N") {
+
+         
+            currNode->right = newNode(stoi(currVal));
+
+            
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        Solution ob;
+        vector<int> vec = ob.rightView(root);
+        for(int x : vec){
+            cout<<x<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
